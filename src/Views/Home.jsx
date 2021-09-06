034e86190db4fbf9c5f473077afbba75209d9341
David Gonzalez <david.gonzalez@shopery.com>
2021-09-06 08:38:49 +0100

    add new section roadmap and header

---
+++ src/Views/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Roadmap from "../Components/Roadmap";
+
+const Home = () => {
+	return(
+		<>
+		<Header/>
+		<Roadmap/>
+		</>
+	)
+}
+export default Home
