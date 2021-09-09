eff4c9e12cd48705337c99a6fc3b533d6be4dbf8
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-09 19:39:30 +0100

    create view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Router} from '@reach/router'
 import Home from './views/Home';
+import Mint from './views/Mint';
 
 const App = ()=>  {
   return (
     <Router>
 		  <Home path="/" />
+      <Mint path="/mint"/>
 	  </Router>
   );
 }
---
+++ src/views/Mint.tsx
@@ -0,0 +1,6 @@
+import { RouteComponentProps } from "@reach/router"
+
+const Mint = (props: RouteComponentProps) => {
+  return <></>
+}
+export default Mint
