52ff85ed5fd777b517b076f1ae597370db0fb25c
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-09 21:30:34 +0100

    remove mint page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Router } from "@reach/router";
 import WalletProvider from "./components/WalletProvider";
 import Home from "./views/Home";
-import Mint from "./views/Mint";
 
 const App = () => {
   return (
     <WalletProvider>
       <Router>
         <Home path="/" />
-        <Mint path="/mint" />
       </Router>
     </WalletProvider>
   );
--- src/views/Mint.tsx
+++
@@ -1,6 +0,0 @@
-import { RouteComponentProps } from "@reach/router"
-
-const Mint = (props: RouteComponentProps) => {
-  return <></>
-}
-export default Mint
