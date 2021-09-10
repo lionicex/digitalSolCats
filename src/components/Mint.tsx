cec52ab2c063d395876ce82c63c94c3f93a42868
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-10 10:06:04 +0100

    add simple modal to Mint

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import ReactModal from "react-modal";
+import "../styles/mint.css";
+
+ReactModal.setAppElement("#root");
+ReactModal.defaultStyles.overlay = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, 0.3)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+ReactModal.defaultStyles.content = {
+  border: "1px solid #ccc",
+  background: "#fff",
+  overflow: "auto",
+  WebkitOverflowScrolling: "touch",
+  borderRadius: "4px",
+  outline: "none",
+  padding: "20px",
+};
+
 const Mint = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
+      <button onClick={() => setIsModalOpen(true)}>Mint now</button>
+      <ReactModal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Mint Modal"
+        shouldCloseOnOverlayClick={false}
+      >
+        <button onClick={() => setIsModalOpen(false)}>Cancel</button>
+      </ReactModal>
     </>
   );
 };
