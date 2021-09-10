a331bcc70099f5b949f866f4850b5896aed1a4f3
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-10 22:37:53 +0100

    remove countdown from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,49 @@
-import { CSSProperties } from 'react'
-import '../styles/header.css'
-import Countdown from './Countdown'
-import WalletButton from './WalletButton'
+import { CSSProperties } from "react";
+import "../styles/header.css";
+import WalletButton from "./WalletButton";
 
 const Header = () => {
-	const header: CSSProperties = {
-		position: 'relative',
-		height: '100vh',
-		padding: '48px',
-		background: 'linear-gradient(0deg,#e7f5ff,#d0f7ff)',
-		overflow: 'hidden'
-	}
-	const buttonHeader: CSSProperties = {
-		display: 'flex',
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-		zIndex: 50
-	}
-	const buttons: CSSProperties = {
-		marginLeft: '32px'
-	}
-	const wallet: CSSProperties = {
-		position: 'relative'
-	}
-	return (
-		<div style={header}>
-			<div style={buttonHeader}>
-				<a href="https://twitter.com" style={buttons}>twitter</a>
-				<a href="https://discord.com" style={buttons}>Discord</a>
-				<div style={wallet}>
-					<WalletButton />
-				</div>
-			</div>
-			<h1 className="title">ZebraPunk</h1>
-			<h3 className="subtitle">10,000 cute & cruel zebras living on the Solana lands. Each of them are randomly
-				generated with more than 90+ hand drawn traits!</h3>
-			<p className="text text-caption">Vamos a vender fleje</p>
-			<Countdown />
-			<div className="bottom-image"/>
-		</div>
-	)
-}
-export default Header
+
+  const header: CSSProperties = {
+    position: "relative",
+    height: "100vh",
+    padding: "48px",
+    background: "linear-gradient(0deg,#e7f5ff,#d0f7ff)",
+    overflow: "hidden",
+  };
+  const buttonHeader: CSSProperties = {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    zIndex: 50,
+  };
+  const buttons: CSSProperties = {
+    marginLeft: "32px",
+  };
+  const wallet: CSSProperties = {
+    position: "relative",
+  };
+  return (
+    <div style={header}>
+      <div style={buttonHeader}>
+        <a href="https://twitter.com" style={buttons}>
+          twitter
+        </a>
+        <a href="https://discord.com" style={buttons}>
+          Discord
+        </a>
+        <div style={wallet}>
+          <WalletButton />
+        </div>
+      </div>
+      <h1 className="title">ZebraPunk</h1>
+      <h3 className="subtitle">
+        10,000 cute & cruel zebras living on the Solana lands. Each of them are
+        randomly generated with more than 90+ hand drawn traits!
+      </h3>
+      <p className="text text-caption">Vamos a vender fleje</p>
+      <div className="bottom-image" />
+    </div>
+  );
+};
+export default Header;
