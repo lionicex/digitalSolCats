1fff99f1467551e1311a966e803b6e795f1bc6d8
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-12 16:30:08 +0100

    add dev changes

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,30 +1,44 @@
-import '../styles/team.css'
+import "../styles/team.css";
 const center = {
-	alignSelf: 'center'
-}
+  alignSelf: "center",
+};
 const Team = () => {
-	return (
-		<div className="TeamRoot"><h2 className="Text Text-title" style={center}>The Team</h2>
-			<div className="TeamMembers">
-				<div className="TeamMember">
-					<img className="TeamImage" alt="Piggy Bank" src="/static/media/team-pb.44ebe4b3.png"/>
-					<h3 className="Text Text-subtitle">Piggy Bank</h3>
-					<p className="Text Text-caption">DEVELOPER</p>
-				</div>
-				<div className="TeamMember">
-					<img className="TeamImage" alt="Piggy Capone" src="/static/media/team-pc.5ba6444d.png"/>
-					<h3 className="Text Text-subtitle">Piggy Capone</h3>
-					<p className="Text Text-caption">PRODUCT</p>
-				</div>
-				<div className="TeamMember"><
-					img className="TeamImage" alt="Pigasso" src="/static/media/team-p.b63a984e.png"/>
-					<h3
-						className="Text Text-subtitle">Pigasso</h3>
-					<p className="Text Text-caption">ARTIST</p>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="TeamRoot">
+      <h2 className="Text-team Text-title-team" style={center}>
+        The Team
+      </h2>
+      <div className="TeamMembers">
+        <div className="TeamMember">
+          <img
+            className="TeamImage"
+            alt="Piggy Bank"
+            src="/static/media/team-pb.44ebe4b3.png"
+          />
+          <h3 className="Text-team Text-subtitle-team">Piggy Bank</h3>
+          <p className="Text-team Text-caption-team">DEVELOPER</p>
+        </div>
+        <div className="TeamMember">
+          <img
+            className="TeamImage"
+            alt="Piggy Capone"
+            src="/static/media/team-pc.5ba6444d.png"
+          />
+          <h3 className="Text-team Text-subtitle-team">Piggy Capone</h3>
+          <p className="Text-team Text-caption-team">PRODUCT</p>
+        </div>
+        <div className="TeamMember">
+          <img
+            className="TeamImage"
+            alt="Pigasso"
+            src="/static/media/team-p.b63a984e.png"
+          />
+          <h3 className="Text-team Text-subtitle-team">Pigasso</h3>
+          <p className="Text-team Text-caption-team">ARTIST</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Team
+export default Team;
--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -3,35 +3,15 @@ import {
   WalletDisconnectButton,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import { CSSProperties } from "react";
-
-const walletButton: CSSProperties = {
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  textAlign: "center",
-  padding: "24px",
-  background: "#18acff",
-  border: "2px solid rgba(0,0,0,.1)",
-  boxSizing: "border-box",
-  borderRadius: "16px",
-  fontStyle: "normal",
-  fontWeight: 700,
-  fontSize: "20px",
-  lineHeight: "24px",
-  color: "#fff",
-  marginLeft: "32px",
-  maxHeight: "72px",
-};
 
 const WalletButton = () => {
   const { connected } = useWallet();
   const button = !connected ? (
-    <WalletMultiButton style={walletButton} />
+    <WalletMultiButton className="wallet-button" />
   ) : (
     <WalletDisconnectButton
-      style={{ ...walletButton, background: "#C20114" }}
+      className="wallet-button"
+      style={{ background: "#C20114" }}
     />
   );
 
