976e9d06040a3bb6aea0f3062b4ceacd02762d7f
rubendev04 <66261839+dotalo@users.noreply.github.com>
2021-09-12 17:03:56 +0100

    add solflare to supported wallets

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -3,7 +3,11 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolflareWebWallet,
+} from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import { SOLANA_NETWORK_ENV } from "../utils/constants";
@@ -14,12 +18,12 @@ interface WalletProviderProps {
 
 const Wallet = ({ children }: WalletProviderProps) => {
   const endpoint = useMemo(() => clusterApiUrl(SOLANA_NETWORK_ENV), []);
-  const wallets = useMemo(() => [getPhantomWallet()], []);
+  const wallets = useMemo(() => [getPhantomWallet(), getSolflareWallet()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets}>
-        <WalletModalProvider featuredWallets={1}>
+        <WalletModalProvider featuredWallets={2}>
           {children}
         </WalletModalProvider>
       </WalletProvider>
