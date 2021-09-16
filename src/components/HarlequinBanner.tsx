f802826e9dcfec98024ff073358744e708d89276
David Gonzalez <david.gonzalez@shopery.com>
2021-09-16 21:49:55 +0100

    fix css in new banner and team

--- src/components/HarlequinBanner.tsx
+++ src/components/HarlequinBanner.tsx
@@ -1,14 +1,14 @@
 import harlequin from "../static/media/harlequin.png";
 
 const HarlequinBanner = () => {
-  return (
-    <div className="harlequin-banner">
-      <img src={harlequin} alt="Harlequin" />
-      <div>
-        <h3>Catch a Harlequin!</h3>
-        <p>Only 264 IRL, only 264 NFTs</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="harlequin-banner">
+			<img src={harlequin} alt="Harlequin"/>
+			<div>
+				<h3>Catch a Harlequin!</h3>
+				<p>Only 264 IRL, only 264 NFTs</p>
+			</div>
+		</div>
+	);
 };
 export default HarlequinBanner;
